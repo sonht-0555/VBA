ac7e09ba2efb2819402b8375e2cc95658f4b23fa
Son Hoang Tuan <hades190791@gmail.com>
2025-05-12 16:37:00 +0700

    Update app.js

--- app.js
+++ app.js
@@ -389,18 +389,20 @@ loadButton.addEventListener('click', async function() {
     Module._emuLoadState(loadStateBufferPtr, dataSize);
     Module._free(loadStateBufferPtr);
 });
-indexedDB.open('/data').onsuccess = (e) => {
-    const db = e.target.result;
-    const range = IDBKeyRange.bound('/data/states/', '/data/states/' + '\uffff');
-    db.transaction('FILE_DATA', 'readonly').objectStore('FILE_DATA').openCursor(range).onsuccess = (e) => {
-        const cursor = e.target.result;
-        if (cursor) {
-        const key = cursor.key;
-        console.log(key.substring(key.lastIndexOf('/') + 1));
-        cursor.continue();
-        }
-    };
-    };
+function loadfile() {
+    indexedDB.open('/data').onsuccess = (e) => {
+        const db = e.target.result;
+        const range = IDBKeyRange.bound('/data/states/', '/data/states/' + '\uffff');
+        db.transaction('FILE_DATA', 'readonly').objectStore('FILE_DATA').openCursor(range).onsuccess = (e) => {
+          const cursor = e.target.result;
+          if (cursor) {
+            const key = cursor.key;
+            console.log(key.substring(key.lastIndexOf('/') + 1));
+            cursor.continue();
+          }
+        };
+      };
+}
 function create() {
     const request = indexedDB.open('/data', 1); // Tạo hoặc mở cơ sở dữ liệu với version 1
 
